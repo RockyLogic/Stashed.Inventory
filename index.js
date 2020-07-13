3a5d098bafa4b7ab075b88fc89ec197f7bf5ff09
Rkuangdev <RKuangDev@gmail.com>
2020-07-13 02:37:39 -0400

    add landing page html and css

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var express = require("express"),
     app = express(),
-    sendToLogs = require("./misc/sendToLog")
+    sendToLogs = require("./misc/sendToLog"),
+    favicon = require('serve-favicon'),
+    path = require('path')
 
 port = process.env.PORT || 3000
 
+app.use(favicon(path.join(__dirname, "public", "images", "Icon2.png")))
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "./public"));
 
+
 app.get("/", (req, res) => {
-    res.render("landing")
+    res.sendFile(__dirname + "/views/landing.html")
 })
 
 app.listen(port, () => {
