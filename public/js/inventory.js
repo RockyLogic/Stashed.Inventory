e9f4a7cadbfe854e0817511123ec5f0d39c58a75
Rkuangdev <RKuangDev@gmail.com>
2020-08-16 02:30:25 -0400

    animation edit

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -3,9 +3,9 @@
 $('.deleteItemButton').click(function () {
     const itemId = $(this).attr('id')
     // fades out
-    $(this).parent().parent().parent().fadeOut(300, function () {
+    $(this).parent().parent().parent().fadeOut(200, function () {
         //smooth transition for divs underneath
-        $(this).css({ "visibility": "hidden", display: 'block' }).slideUp();
+        $(this).css({ "visibility": "hidden", display: 'block' }).slideUp(150);
     });
     $.ajax({
         method: 'POST',
@@ -19,9 +19,9 @@ $('.deleteItemButton').click(function () {
 $('.deleteSaleButton').click(function () {
     const saleId = $(this).attr('id')
     // fades out
-    $(this).parent().parent().parent().fadeOut(300, function () {
+    $(this).parent().parent().parent().fadeOut(200, function () {
         //smooth transition for divs underneath
-        $(this).css({ "visibility": "hidden", display: 'block' }).slideUp();
+        $(this).css({ "visibility": "hidden", display: 'block' }).slideUp(150);
     });
     $.ajax({
         method: 'POST',
@@ -30,3 +30,28 @@ $('.deleteSaleButton').click(function () {
         console.log('item deleted: ' + saleId)
     })
 })
+
+// // cloning item with ajax (need to display new item)
+// $('.cloneItemButton').click(function () {
+//     const itemId = $(this).attr('id')
+//     $.ajax({
+//         method: 'POST',
+//         url: '/item/' + itemId + "/clone",
+//         success: function (resp) {
+//             console.log('Item Cloned: ' + resp._id)
+//             const item = resp
+
+//             let str = "<%= include('/itemsDisplay', {item: resp}); %>",
+//                 fn = ejs.compile(str, { client: true });
+
+//             fn(data, null, function (path, d) { // include callback
+//                 console.log(path);
+//                 console.log(d);
+//                 // d -> {person: 'John'}
+//                 // Put your code here
+//                 // Return the contents of file as a string
+//             }); // returns rendered string
+//             $('.inventory-scroll-list').append(fn)
+//         },
+//     })
+// })
