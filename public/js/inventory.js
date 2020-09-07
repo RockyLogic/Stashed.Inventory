05591b1523da99b0ca984b528ecfe6c8355a271a
Rkuangdev <RKuangDev@gmail.com>
2020-09-07 19:03:25 -0400

    re:re:fixed animation issues

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -8,7 +8,8 @@ $(function () {
         Observer = new IntersectionObserver((entries) => {
             entries.forEach((entry) => {
                 if (entry.intersectionRatio > 0) {
-                    entry.target.style.animation = `animation1 1s ${entry.target.dataset.delay} both ease-out`
+                    entry.target.style.animation = `animation1 1s ${entry.target.dataset.delay} ease-out`
+                    setTimeout(function () { entry.target.classList.remove("init") }, 1000);
                 }
             })
         })
